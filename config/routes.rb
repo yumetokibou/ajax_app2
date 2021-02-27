3ce2e2ff1be095ef9618df14550b369d8dfbfd1d
yumetokibou <77270061+yumetokibou@users.noreply.github.com>
2021-02-27 12:06:50 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'posts#index' # ←編集
 # get 'posts/new', to: 'posts#new' ←削除
   post 'posts', to: 'posts#create'
+  get 'posts/:id', to: 'posts#checked'
 end
